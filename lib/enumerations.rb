c3eca40b768dc5b8e86e53abad0f924ef3650425
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2020-02-21 09:49:09 +0100

    Change to Integer

--- lib/enumerations.rb
+++ lib/enumerations.rb
@@ -3,30 +3,30 @@ require 'active_support/core_ext/string/inflections.rb'
 
 module Enumeration
   VERSION = "1.1"
-  
+
   def self.included(receiver)
     receiver.extend ClassMethods
   end
-  
+
   module ClassMethods
     # create an enumeration for the symbol <tt>name</tt>. Options include <tt>foreign key</tt> attribute and Class name
     def enumeration(name, options = {})
       options[:foreign_key] ||= "#{name}_id".to_sym
       options[:class_name] ||= name.to_s.camelize
-      
+
       # getter for belongs_to
       define_method name do
         options[:class_name].constantize.find(send(options[:foreign_key]))
       end
-      
-      # setter for belongs_to 
+
+      # setter for belongs_to
       define_method "#{name}=" do |other|
         send("#{options[:foreign_key]}=", other.id)
       end
-      
+
       # store a list of used enumerations
       @_all_enumerations ||= []
-      @_all_enumerations << EnumerationReflection.new(name, options)            
+      @_all_enumerations << EnumerationReflection.new(name, options)
     end
 
     # output all the enumerations that this model has defined
@@ -34,27 +34,27 @@ module Enumeration
       @_all_enumerations
     end
   end
-  
+
   class EnumerationReflection < Struct.new(:name, :options)
     def class_name
       options[:class_name]
     end
-    
+
     def foreign_key
       options[:foreign_key]
     end
-  end  
-  
+  end
+
   # used as a base class for enumeration classes
   class Base < Struct.new(:id, :name, :symbol)
     class_attribute :all, :id_index, :symbol_index
-    
+
     def singleton_class
       class << self
         self
       end
-    end    
-    
+    end
+
     def self.values(values)
       # all values
       self.all = []
@@ -62,7 +62,7 @@ module Enumeration
       self.id_index = {}
       # for symbol based lookup
       self.symbol_index = {}
-      
+
       values.each_pair do |symbol, attributes|
         attributes[:name] ||= symbol.to_s.humanize
         object = self.new(attributes[:id], attributes[:name], symbol)
@@ -70,28 +70,28 @@ module Enumeration
         self.singleton_class.send(:define_method, symbol) do
           object
         end
-        
+
         raise "Duplicate id #{attributes[:id]}" if self.id_index[attributes[:id]]
-        raise "Duplicate symbol #{symbol}" if self.symbol_index[symbol]        
-        
+        raise "Duplicate symbol #{symbol}" if self.symbol_index[symbol]
+
         self.id_index[attributes[:id]] = object
         self.symbol_index[symbol] = object
         self.all << object
       end
     end
-    
+
     # lookup a specific enum
     def self.find(id)
-      if id.is_a?(Fixnum)
+      if id.is_a?(Integer)
         self.id_index[id]
       elsif id.is_a?(Symbol) || id.is_a?(String)
         self.symbol_index[id.to_sym]
       end
     end
-    
+
     # FIXME for some reason this doesn't work for case..when expressions
-    def ==(object)  
-      if object.is_a?(Fixnum)
+    def ==(object)
+      if object.is_a?(Integer)
         object == self.id
       elsif object.is_a?(Symbol)
         object == self.symbol
@@ -99,24 +99,24 @@ module Enumeration
         object.id == self.id
       end
     end
-    
+
     # TODO alias method??
     def to_s
       name
-    end  
-    
+    end
+
     def to_i
       id
     end
-    
+
     def to_sym
-      symbol 
+      symbol
     end
-    
+
     def to_param
-      id  
+      id
     end
-    
+
     def method_missing(method_name, *args)
       symbol = method_name.to_s.gsub(/[?]$/, '')
       if self.class.find(symbol) && method_name =~ /[?]$/
